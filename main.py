90b9d29ad1941f12f2a354bb1f1bc31217107741
Julio Nuñez <49283373+yulsnp@users.noreply.github.com>
2023-10-03 23:07:02 -0600

    Update main.py
    Updated code to get areas of triangle, circle and square.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-#Instructions: Create a copy of the original list "lst1" using list comprehensions
-lst1=[1,2,3,4,5]
-lst2=[i for i in lst1]
-print(lst2)
+def area_triangle(b,h):
+    #Add your code here
+
+def area_square(side):
+    #Add your code here
+
+def area_circle(radius):
+    #Add your code here
+
+print(f"The area of the triangle is: {}")
+print(f"The area of the square is: {}")
+print(f"The area of the circle is: {}")
