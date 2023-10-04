0dc931b74c1eb3b9561d85455ed4dd55aa3a795a
Julio Nuñez <49283373+yulsnp@users.noreply.github.com>
2023-10-03 23:44:35 -0600

    Create equation.py
    File for new challenge

---
+++ equation.py
@@ -0,0 +1,8 @@
+#Resolve the equation from the associated open issue
+# Test Data
+# Sample input: 1 <-> Expected output: y = 0.6000000000000001
+# Sample input: 10 <-> Expected output: y = 0.09901951266867294
+# Sample input: 100 <-> Expected output: y = 0.009999000199950014
+# Sample input: -5 <-> Expected output: y = -0.19258202567760344
+
+#Write your code here
